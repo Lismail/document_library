d88904e691cbf7a7c7fbb30cbcd5cbb1dfebce57
Lismail <lucky.ismail@yahoo.co.id>
2016-11-17 15:59:57 +0700

    Creating common setting pages.

---
+++ document_library/document_library/doctype/documents/documents.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2016, PT. Mitra Computa Asia, Indonesia and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Documents', {
+	refresh: function(frm) {
+
+	}
+});
