a0a9e761854109290e39ae5710aac47932b57a83
Lismail <lucky.ismail@yahoo.co.id>
2016-11-18 15:04:09 +0700

    Creating Document Transaction doctype

---
+++ document_library/document_library/doctype/document_transactions/test_document_transactions.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2015, PT. Mitra Computa Asia, Indonesia and Contributors
+# See license.txt
+from __future__ import unicode_literals
+
+import frappe
+import unittest
+
+# test_records = frappe.get_test_records('Document Transactions')
+
+class TestDocumentTransactions(unittest.TestCase):
+	pass
